4366b3b14040877ee8901b6d1847055738932f1d
Hamza Raza-75 <hamzarazakhan2323@gmail.com>
2025-04-15 00:37:18 +0500

    Till there databaseconnection has been submitted successfully

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import app from './app.js';
 import { PORT, corsOptions } from './src/config/index.js';
 import cors from 'cors';
+import { dbConnection } from './src/app/services/dbconnect.js';
 
-app.use(cors());
+app.use(cors()); //here we have to configure the cors options which are already imported on up
+// app.use('/api');
+dbConnection();
 
 app.listen(PORT, () => {
   console.log('server is listening at 3000');
--- src/config/common.config.js
+++ src/config/common.config.js
@@ -2,7 +2,10 @@ const env = process.env;
 
 export const PORT = env.PORT || '3000';
 export const NODE_ENV = env.NODE_ENV || 'development';
+export const DATABASE_CONNECTION =
+  env.DATABASE_CONNECTION || 'mongodb://127.0.0.1:27017/test';
 export const corsOptions = {
   origin: env.BACKEND_ORIGIN,
+  credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
